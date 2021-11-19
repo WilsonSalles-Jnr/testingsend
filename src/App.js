fefd0316e3ef1546e117facc339ab562892758e9
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-11-19 13:47:54 -0300

    criando modelo skills

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Pages/Home';
+import Skills from './Pages/Skills';
 
 class App extends React.Component {
   render() { 
     return (
       <>
       <Header />
-      <Home />
+      {/* <Home /> */}
+      <Skills />
       <Footer />
       </>
     );
---
+++ src/Pages/Skills.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Skills extends React.Component {
+  render() { 
+    return <section>
+      <h1>Skills</h1>
+      <hr />
+      <h2>Fundamentais</h2>
+      <ul>
+        <li>Git</li>
+        <li>Github</li>
+        <li>Javascript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Bootstrap</li>
+        <li>JQuery</li>
+      </ul>
+      <h2>Front-end</h2>
+      <ul>
+        <li>ReactJS</li>
+        <li>React Testing Library (RTL)</li>
+        <li>Redux</li>
+        <li>ContextAPI</li>
+      </ul>
+    </section>;
+  }
+}
+ 
+export default Skills;
